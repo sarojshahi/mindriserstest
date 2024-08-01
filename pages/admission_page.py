be5b4952d85ba6695625ae7c9039fcbb59886439
Saroj Shahi <saroj.shai.c4@gmail.com>
2024-08-01 13:10:41 +0545

    DROPDOWN CONQUERED

--- pages/admission_page.py
+++ pages/admission_page.py
@@ -1,5 +1,6 @@
 #import all the necessary modules
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 
 #define class for the admission page
 class Admission:
@@ -9,6 +10,9 @@ class Admission:
         self.email_textbox = By.XPATH,"//input[@id='email']"
         self.phone_textbox = By.XPATH,"//input[@id='mobile_no']"
         self.college_textbox = By.XPATH,"//input[@id='college']"
+        self.academic_status_dropdown = (By.ID, 'qualification')
+        self.interest= (By.ID,'course')
+        self.preferred_schedule = (By.ID,'shedule')
         self.queries_textbox = By.XPATH,"//textarea[@id='question']"
     def open_page(self,url):
         self.driver.get(url)
@@ -24,6 +28,21 @@ class Admission:
 
     def enter_college(self,college):
         self.driver.find_element(*self.college_textbox).send_keys(college)
+
+    def enter_academic_qualification(self):
+        academics = self.driver.find_element(*self.academic_status_dropdown)
+        select1 = Select(academics)
+        select1.select_by_index(1)
+
+    def enter_interest(self):
+        interested_course = self.driver.find_element(*self.interest)
+        select2 = Select(interested_course)
+        select2.select_by_index(4)
+
+    def enter_schedule(self):
+        schedule = self.driver.find_element(*self.preferred_schedule)
+        select3 = Select(schedule)
+        select3.select_by_index(2)
 
     def enter_queries(self, queries):
         self.driver.find_element(*self.queries_textbox).send_keys(queries)
--- pages/dashboard_page.py
+++ pages/dashboard_page.py
@@ -2,12 +2,14 @@
 #import the necessary modules
 from selenium.webdriver.common.by import By
 import time
+from selenium.webdriver.support.ui import Select
 
 #define class for the dashboard page
 class DashboardPage:
     def __init__(self,driver):
         self.driver = driver
         self.banner_button = By.XPATH,"//span[@class='absolute right-0 top-0 flex h-[28px] w-[28px] -translate-y-1/2 translate-x-1/2 cursor-pointer items-center justify-center rounded-full bg-red-400 font-bold text-white md:h-[32px] md:w-[32px] ']"
+        self.courses = (By.ID, 'course')
         self.name_field = By.XPATH,"//input[@placeholder='Full Name']"
         self.email_field = By.XPATH,"//input[@placeholder='Email']"
         self.phone_field = By.XPATH,"//input[@placeholder='Phone Number']"
@@ -31,6 +33,13 @@ class DashboardPage:
             self.driver.execute_script(f"window.scrollBy(0,{scroll_distance});")
             current_y += scroll_distance
             time.sleep(2)
+
+    def enter_interest(self):
+        interested_course = self.driver.find_element(*self.courses)
+        select = Select(interested_course)
+        select.select_by_index(4)
+
+
 
     def enter_name(self,name):
         self.driver.find_element(*self.name_field).send_keys(name)
